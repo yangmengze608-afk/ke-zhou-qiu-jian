40b63a95e75fcc8a63a964859df81774a66b0822
Ykmmz <yangmengze608@gmail.com>
2026-09-01 16:01:36 +1000

    style: load living river interactions

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './flow.css'
+import './living-river.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode><App /></StrictMode>,
