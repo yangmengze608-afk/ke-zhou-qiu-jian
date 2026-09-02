3a6ef6ad5095af56a6bd901dba65a52c4bf1827a
Ykmmz <yangmengze608@gmail.com>
2026-09-02 16:56:44 +1000

    style: load v06 museum layer

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import './styles.css'
 import './flow.css'
 import './living-river.css'
+import './v6.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode><App /></StrictMode>,
