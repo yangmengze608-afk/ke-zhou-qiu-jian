524c355652f4901c6c447018094f181e30de39ff
Ykmmz <yangmengze608@gmail.com>
2026-09-03 18:45:58 +1000

    style: load TTPlayer concept styles

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import './flow.css'
 import './living-river.css'
 import './v6.css'
 import './revival-score.css'
+import './ttplayer-concept.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode><App /></StrictMode>,
